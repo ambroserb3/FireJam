bf6f133d2cdb968c03ed790278ab3de4b22adef6
gs2947 <35980935+gs2947@users.noreply.github.com>
2018-11-10 17:57:43 -0500

    Add files via upload

---
+++ Fire Jam/Assets/Scripts/HowToButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToButton : MonoBehaviour
+{
+
+
+    public void GoToHowTo()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
