cfc531fcb067a52d3850b3fe7e29083f611c0c56
Jeremy Gillespie <knocking.peoples.hats.off@gmail.com>
2018-11-09 21:51:57 -0500

    Map
    Added map object and tile prefab

---
+++ Fire Jam/Assets/Scripts/Map.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	[SerializeField] private int m_MapSize;
+	[SerializeField] private GameObject m_TilePrefab;
+	private MapTile[,] grid;
+
+	// Use this for initialization
+	void Awake () {
+		grid = new MapTile[m_MapSize, m_MapSize];
+		for(int i=0; i<m_MapSize; i++){
+			for(int j=0; j<m_MapSize; j++){
+				grid[i,j] = GameObject.Instantiate(m_TilePrefab, new Vector3(i,j,0), Quaternion.identity).GetComponent<MapTile>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
