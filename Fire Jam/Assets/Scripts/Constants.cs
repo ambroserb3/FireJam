f526b8593fe856021345a93cbc4a9e44b712d32b
AlexSissoko <ams2508@columbia.edu>
2018-11-10 13:09:42 -0500

    Some constants to maybe use

--- Fire Jam/Assets/Scripts/Constants.cs
+++ Fire Jam/Assets/Scripts/Constants.cs
@@ -7,10 +7,13 @@ public class Constants : MonoBehaviour {
     public static float sizeScale = 3.2f;
     public static int playerHealth = 100;
     public static int towerHealth = 1000;
+    //There should also be default spawn locations from both players.
+    //Probly just a tuple of x,y coordinates. Not sure how we want to 
+    //Model it yet.
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
