547715227f815fdf265e8cf0f01695b01287fe1b
Jeremy Gillespie <knocking.peoples.hats.off@gmail.com>
2018-11-10 12:06:47 -0500

    Merged

--- Fire Jam/Assets/Scripts/Map.cs
+++ Fire Jam/Assets/Scripts/Map.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,42 +30,3 @@ public class Map : MonoBehaviour {
 		tile.Init(x, y, grid);
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Map : MonoBehaviour {
-
-    [SerializeField] public int m_MapSize;
-    [SerializeField] public GameObject m_TilePrefab;
-
-    public GameManager GM;
-
-    public MapTile[] tileList;
-
-	private MapTile[,] grid;
-
-    // Use this for initialization
-    private void Awake() {
-
-    }
-
-    void Start() {
-		grid = new MapTile[m_MapSize, m_MapSize];
-		for(int i=0; i<m_MapSize; i++){
-			for(int j=0; j<m_MapSize; j++){
-                int x = Random.Range(0, tileList.Length);
-                grid[i,j] = Instantiate(tileList[x], new Vector3(i,j,0), Quaternion.identity).GetComponent<MapTile>();
-                grid[i,j].SetPos(i,j);
-                grid[i,j].GM = GM;
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> d3b65dd587470221b4b00f02ae882e7cbb257a8f
--- Fire Jam/Assets/Scripts/MapTile.cs
+++ Fire Jam/Assets/Scripts/MapTile.cs
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapTile : MonoBehaviour {
-
-	public MapTile[,] m_grid;
-	public int m_x;
-	public int m_y;
-
-	public void Init(int x, int y, MapTile[,] grid){
-		m_x = x;
-		m_y = y;
-		m_grid = grid;
-	}
-
-	// Use this for initialization
-	public List<MapTile> GetAdjacent(){
-		List<MapTile> res = new List<MapTile>();
-		if(m_x>0)
-			res.Add(m_grid[m_x-1, m_y]);
-		if(m_y>0)
-			res.Add(m_grid[m_x, m_y-1]);
-		if(m_x<m_grid.GetUpperBound(0))
-			res.Add(m_grid[m_x+1, m_y]);
-		if(m_y<m_grid.GetUpperBound(1));
-			res.Add(m_grid[m_x, m_y+1]);
-		return res;
-	}
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -78,4 +46,3 @@ public class MapTile : MonoBehaviour {
         this.y = y;
     }
 }
->>>>>>> d3b65dd587470221b4b00f02ae882e7cbb257a8f
