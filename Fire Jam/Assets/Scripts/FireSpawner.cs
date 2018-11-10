f9bddf2e9696b8d664ba6bbb1fa79d62e4372405
Jeremy Gillespie <knocking.peoples.hats.off@gmail.com>
2018-11-10 14:56:03 -0500

    Fire Spread

--- Fire Jam/Assets/Scripts/FireSpawner.cs
+++ Fire Jam/Assets/Scripts/FireSpawner.cs
@@ -6,13 +6,23 @@ public class FireSpawner : MonoBehaviour {
 
 	private MapTile[,] grid;
 
+	
+	public int SpreadFreq = 1;
+	private float SpreadRate;
+
 	void Awake () {
 		grid = GameObject.FindWithTag("Map").GetComponent<Map>().grid;
+		SpreadRate = 1f/SpreadFreq;
+		StartCoroutine(Spread());
 	}
+
 	
-	void FixedUpdate () {
-		int x = (int)(transform.position.x+0.5f);
-		int y = (int)(transform.position.y+0.5f);
-		grid[x,y].Ignite();
+	IEnumerator Spread(){
+		while(true){
+			int x = (int)(transform.position.x+0.5f);
+			int y = (int)(transform.position.y+0.5f);
+			grid[x,y].Burn();
+			yield return new WaitForSeconds(SpreadRate);
+		}
 	}
 }
--- Fire Jam/Assets/Scripts/MapTile.cs
+++ Fire Jam/Assets/Scripts/MapTile.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class MapTile : MonoBehaviour {
 
     public GameObject FirePrefab;
-    public float igniteTime;
-    public int maxHealth; //how many spurts of water it needs to douse
+    public int igniteTime;
+    public int extinguishHP; //how many spurts of water it needs to douse
 
     public bool isLit = false;
     public MapTile[,] grid;
 
     public GameManager GM;
 
-    private int health;
+    private int waterHealth;
+    private int fireHealth;
 
     private int x;
     private int y;
 
 	void Awake () {
+        waterHealth = extinguishHP;
+        fireHealth = igniteTime;
 	}
-	
+
+    public void Burn(){
+        fireHealth-=1;
+        if(fireHealth==0)
+            Ignite();
+    }
 
     public void Ignite(){
         if(!isLit){
@@ -35,4 +43,17 @@ public class MapTile : MonoBehaviour {
         this.y = y;
         this.grid = grid;
     }
+
+    public List<MapTile> GetAdjacent(){
+		List<MapTile> res = new List<MapTile>();
+		if(x>0)
+			res.Add(grid[x-1, y]);
+		if(y>0)
+			res.Add(grid[x, y-1]);
+		if(x<grid.GetUpperBound(0))
+			res.Add(grid[x+1, y]);
+		if(y<grid.GetUpperBound(1))
+			res.Add(grid[x, y+1]);
+		return res;
+	}
 }
