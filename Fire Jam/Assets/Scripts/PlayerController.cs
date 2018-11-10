6d7b7bf49082a4e90b3ebf8ff2423fb9b1e27ecd
Jeremy Gillespie <knocking.peoples.hats.off@gmail.com>
2018-11-09 20:54:02 -0500

    Main Scene
    Created Main scene with player objects

---
+++ Fire Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel.
+	[SerializeField] private float m_Accel = .1f;
+
+	private Animator m_Anim;            // Reference to the player's animator component.
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+	private void Awake()
+	{
+		m_Anim = GetComponent<Animator>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+
+	private void FixedUpdate()
+	{
+	}
+
+
+	public void Move(Vector2 move)
+	{
+		// Move the character
+		m_Rigidbody2D.AddForce(move*m_Accel);
+		if(m_Rigidbody2D.velocity.magnitude > m_MaxSpeed){
+			m_Rigidbody2D.velocity = m_Rigidbody2D.velocity.normalized*m_MaxSpeed;
+		}
+
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}	
---
+++ Fire Jam/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerInput : MonoBehaviour {
+
+	private PlayerController m_Controller;
+
+	private void Awake()
+	{
+		m_Controller = GetComponent<PlayerController>();
+	}
+
+
+	private void Update()
+	{
+
+	}
+
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		Vector2 direction = new Vector2();
+		if(CrossPlatformInputManager.GetButton("left"))
+			direction += new Vector2(-1,0);
+		if(CrossPlatformInputManager.GetButton("right"))
+			direction += new Vector2(1,0);
+		if(CrossPlatformInputManager.GetButton("up"))
+			direction += new Vector2(0,1);
+		if(CrossPlatformInputManager.GetButton("down"))
+			direction += new Vector2(0,-1);
+		// Pass all parameters to the character control script.
+		m_Controller.Move(direction.normalized);
+	}
+}
