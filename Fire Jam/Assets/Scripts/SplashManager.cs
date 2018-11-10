10ee27d732b4fedb0a609345be0c52c8ceec95fd
stephanrb3 <stephanrb3@gmail.com>
2018-11-10 13:33:31 -0500

    Network Identities

---
+++ Fire Jam/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour {
+    public GameObject splashmanager;
+
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(splashmanager);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+}
